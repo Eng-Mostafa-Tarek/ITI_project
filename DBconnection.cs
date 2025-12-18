153f2b59cd4c203ee88a575cf0c6d42fa636de9d
negoo <nagatgamal485@gmail.com>
2025-12-18 17:44:21 +0200

    Add files via upload

---
+++ DBconnection.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_windows.DB
+{
+    internal class DBconnection
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string connectionString =
+                "server=localhost;database=LibraryDB;user=root;password=;root123";
+
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
